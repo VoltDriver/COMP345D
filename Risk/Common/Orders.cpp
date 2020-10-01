d09df558d443cd2f10cf2ccad76f11fac7044fd7
Tayyab <tayyab.h.qureshi@gmail.com>
2020-09-30 22:39:52 -0400

    Added functionality for Orders.cpp and Orders.h

--- Risk/Common/Orders.cpp
+++ Risk/Common/Orders.cpp
@@ -1,3 +1,75 @@
 #include "Orders.h"
+#include <iterator>
+#include <iostream>
 
-// This is a placeholder. This needs to be coded.
+using namespace std;
+
+Orders:: Orders(){
+    id = 0;
+}
+
+Deploy::Deploy(){
+    id = 1;
+}
+
+Advance::Advance() {
+    id = 2;
+}
+
+Bomb::Bomb(){
+    id = 3;
+}
+
+Blockade::Blockade() {
+    id = 4;
+}
+
+Airlift::Airlift() {
+    id = 5;
+}
+
+Negotiate::Negotiate() {
+    id = 6;
+}
+
+OrdersList::OrdersList(vector<Orders> &list){
+    myList = &list;
+
+}
+
+
+void OrdersList::remove(Orders order){
+    for(int i =0; i<myList->size(); i++ ){
+        if (myList->at(i).id == order.id)
+            myList->erase(myList->begin()+i);
+    }
+}
+
+void OrdersList::move(Orders order){
+    std::vector<Orders>:: iterator i;
+    for(i = myList->begin(); i != myList->end(); i++){
+
+    }
+}
+
+void OrdersList::add(Orders order){
+    myList->push_back(order);
+}
+
+
+
+/*int main(){
+
+    vector<Orders> list1;
+    Orders order1;
+    Deploy deploy1;
+    OrdersList ol(list1);
+    ol.add(deploy1);
+    ol.add(order1);
+    //vector<Orders> &vec1 = ol.myList;
+    cout << ol.myList->at(0).id << endl;
+    cout << ol.myList->size() << endl;
+    ol.remove(deploy1);
+    cout << ol.myList->at(0).id << endl;
+    cout << ol.myList->size() << endl;
+}*/
--- Risk/Common/Orders.h
+++ Risk/Common/Orders.h
@@ -1,10 +1,69 @@
-#ifndef COMP345RISK_ORDERS_H
-#define COMP345RISK_ORDERS_H
+#include <list>
+#include <vector>
+using namespace std;
+
+#ifndef UNTITLED_ORDERS_H
+#define UNTITLED_ORDERS_H
 
 
-class Order {
-    // This is a placeholder. This needs to be coded.
+class Orders {
+public:
+    Orders();
+
+    int id;
+    bool validate();
+    void execute();
+
+
+};
+
+class Deploy: public Orders{
+public:
+    Deploy();
+
+};
+
+class Advance:public Orders{
+public:
+    Advance();
+};
+
+class Bomb:public Orders{
+public:
+    Bomb();
+};
+
+class Blockade:public Orders{
+public:
+    Blockade();
 };
 
 
-#endif //COMP345RISK_ORDERS_H
+class Airlift:public Orders{
+public:
+    Airlift();
+};
+
+class Negotiate:public Orders{
+public:
+    Negotiate();
+};
+
+
+class OrdersList{
+public:
+    OrdersList(vector<Orders> &list);
+    int id;
+
+    void remove(Orders order);
+    void move(Orders order);
+    void add(Orders order);
+
+    vector<Orders> *myList;
+
+
+
+};
+
+
+#endif //UNTITLED_ORDERS_H
