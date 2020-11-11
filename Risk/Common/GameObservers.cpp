eebf5fa74a1632b1b8906123e29cf46d5498b038
Jimmy Wong <jimmy-6610@hotmail.com>
2020-11-11 01:18:07 -0500

    Implemented Observer and Subject classes

--- Risk/Common/GameObservers.cpp
+++ Risk/Common/GameObservers.cpp
@@ -1,3 +1,30 @@
-
-
 #include "GameObservers.h"
+
+
+/* Observer Class */
+Observer::Observer(){}
+Observer::~Observer(){}
+
+
+/* Subject Class */
+Subject::Subject(){
+    observer_list = new list<Observer*>;
+}
+
+Subject::~Subject(){
+    delete observer_list;
+}
+
+void Subject::add(Observer* o){
+    observer_list->push_back(o);
+}
+
+void Subject::remove(Observer* o){
+    observer_list->remove(o);
+}
+
+void Subject::notify(){
+    list <Observer*>::iterator i = observer_list->begin();
+    for (; i != observer_list->end(); ++i)
+        (*i)->Update();
+}
--- Risk/Common/GameObservers.h
+++ Risk/Common/GameObservers.h
@@ -1,12 +1,35 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+
+using namespace std;
 
 
-#ifndef COMP345RISK_GAMEOBSERVERS_H
-#define COMP345RISK_GAMEOBSERVERS_H
-
-
-class GameObservers {
-
+/**
+ * The Observer class is basically an interface or abstract class since it contains at least one pure virtual function.
+ * Any observer that is created will have to implement its own Update().
+ */
+class Observer {
+public:
+	virtual void Update() = 0;   // pure virtual function. This forces any subclass to implement Update()
+    ~Observer();
+protected:
+    Observer();
 };
 
 
-#endif //COMP345RISK_GAMEOBSERVERS_H
+/**
+ *
+ */
+class Subject {
+public:
+    Subject();
+    ~Subject();
+    virtual void add(Observer* o);
+    virtual void remove(Observer* o);
+    virtual void notify();
+private:
+    list <Observer*> *observer_list;
+};
+
