e193babddea5cea009def0f9a1728406bbb70ab2
Jimmy Wong <jimmy-6610@hotmail.com>
2020-11-11 14:07:24 -0500

    Update

--- Risk/Common/GameObservers.h
+++ Risk/Common/GameObservers.h
@@ -5,19 +5,7 @@
 
 using namespace std;
 
-
-/**
- * The Observer class is basically an interface or abstract class since it contains at least one pure virtual function.
- * Any observer that is created will have to implement its own Update().
- */
-class Observer {
-public:
-	virtual void Update() = 0;   // pure virtual function. This forces any subclass to implement Update()
-    ~Observer();
-protected:
-    Observer();
-};
-
+class Observer;
 
 /**
  *
@@ -33,3 +21,26 @@ private:
     list <Observer*> *observer_list;
 };
 
+
+/**
+ * The Observer class is basically an interface or abstract class since it contains at least one pure virtual function.
+ * Any observer that is created will have to implement its own Update().
+ */
+class Observer {
+public:
+    ~Observer();
+	virtual void Update() = 0;   // pure virtual function. This forces any subclass to implement Update()
+protected:
+    Observer();
+};
+
+
+
+class PhaseDisplay : Observer {
+
+};
+
+
+class StatsDisplay : Observer {
+
+};
