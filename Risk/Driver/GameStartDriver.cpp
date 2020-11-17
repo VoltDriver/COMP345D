f03a24b09807186607bb3955db386b865742b670
Steven <ste77236@gmail.com>
2020-11-16 19:50:05 -0500

    Deleted workspace.xml again, fixed cmake, adjusted driver

--- Risk/Driver/GameStartDriver.cpp
+++ Risk/Driver/GameStartDriver.cpp
@@ -10,17 +10,27 @@ int main() {
     };
 
     delete engine;
-
     engine = new GameEngine;
 
-    cout << "\n\n========= Running auto initializer ==========" << endl;
+    cout << "\n\n************ Running auto initializer ************\n" << endl;
+    cout << "*** Running with invalid map ***\n" << endl;
+
+    try {
+        engine->gameStart_Auto("test_bordering_territory_index_error.map", 4, true, true);
+    } catch (exception &e) {
+        cout << "Invalid Map provided, the game will now terminate.\n\n" << endl;
+    }
+
+    delete engine;
+    engine = new GameEngine;
+
+    cout << "*** Running with valid map ***" << endl;
 
     try {
         engine->gameStart_Auto();
     } catch (exception &e) {
         cout << "Invalid Map provided, the game will now terminate." << endl;
-    };
-
+    }
 
     return 0;
 }
