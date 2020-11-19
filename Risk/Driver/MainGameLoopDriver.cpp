696e15888de33dc1ed3f2e7d639ea295223b2fcc
Joel Lajoie-Corriveau <joel.lajoie.corriveau@outlook.com>
2020-11-18 23:05:52 -0500

    Update MainGameLoopDriver.cpp

--- Risk/Driver/MainGameLoopDriver.cpp
+++ Risk/Driver/MainGameLoopDriver.cpp
@@ -7,20 +7,68 @@ int main() {
     // 1) A Player receives the correct number of armies in the reinforcement phase (showing different cases)
     GameEngine engine = GameEngine();
     engine.gameStart_Auto("test_success1.map", 2, true, true);
+    engine.startupPhase();
+
+    for(Player* p : engine.getPlayers())
+    {
+        if(p->name == "Player 1")
+        {
+            p->removeTerritory(p->to_defend().at(0));
+        }
+    }
+
     engine.reinforcementPhase();
 
+    for(Player* p : engine.getPlayers())
+    {
+        cout << p << endl;
+    }
+
     GameEngine engine2 = GameEngine();
-    engine2.gameStart_Auto("test_success1.map", 3, true, true);
+    engine2.gameStart_Auto("test_success2.map", 3, true, true);
+    engine2.startupPhase();
+
+    for(Player* p : engine2.getPlayers())
+    {
+        if(p->name == "Player 1")
+        {
+            p->removeTerritory(p->to_defend().at(0));
+            p->removeTerritory(p->to_defend().at(0));
+            p->removeTerritory(p->to_defend().at(0));
+            p->removeTerritory(p->to_defend().at(0));
+            p->removeTerritory(p->to_defend().at(0));
+            p->removeTerritory(p->to_defend().at(0));
+        }
+    }
+
     engine2.reinforcementPhase();
 
+    for(Player* p : engine2.getPlayers())
+    {
+        cout << p << endl;
+    }
+
     GameEngine engine3 = GameEngine();
-    engine3.gameStart_Auto("test_success1.map", 4, true, true);
+    engine3.gameStart_Auto("test_success3.map", 4, true, true);
+    engine3.startupPhase();
     engine3.reinforcementPhase();
 
+    for(Player* p : engine3.getPlayers())
+    {
+        cout << p << endl;
+    }
+
     GameEngine engine4 = GameEngine();
-    engine4.gameStart_Auto("test_success1.map", 5, true, true);
+    engine4.gameStart_Auto("test_success2.map", 5, true, true);
+    engine4.startupPhase();
     engine4.reinforcementPhase();
 
+    for(Player* p : engine4.getPlayers())
+    {
+        cout << p << endl;
+    }
+
+    // 1 (again) A Player receives the correct number of armies in the reinforcement phase (showing different cases)
     // 2) A player will only issue deploy orders and no other kind of orders if they still have armies in their reinforcement pool.
     // 3) The  game  engine  will  only  execute  non-deploy orders  when  all  the  deploy  orders  of  all  players  have  been  executed
     // 4) A  player  can  issue  advance  orders  to  either  defend  or  attack,  based  on  the toAttack() and toDefend()  lists
